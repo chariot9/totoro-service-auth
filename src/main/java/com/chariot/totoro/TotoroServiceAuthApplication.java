250f93a203a856cfe4d6e84cf14b547d8df5ec5c
Trung Vu <trungvu.inside@gmail.com>
2018-03-28 13:40:31 +0700

    added config yml file

--- src/main/java/com/chariot/totoro/TotoroServiceAuthApplication.java
+++ src/main/java/com/chariot/totoro/TotoroServiceAuthApplication.java
@@ -3,6 +3,7 @@ package com.chariot.totoro;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 /**
@@ -11,6 +12,7 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.E
 @SpringBootApplication
 @EnableResourceServer
 @EnableDiscoveryClient
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class TotoroServiceAuthApplication {
 
     public static void main(String[] args) {
